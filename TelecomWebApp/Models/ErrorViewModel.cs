042842745e9b5a38e3a7363ae609241ad1e289b8
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-11-26 22:38:36 +0200

    Added .NET Core MVC Web App template

---
+++ TelecomWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TelecomWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
