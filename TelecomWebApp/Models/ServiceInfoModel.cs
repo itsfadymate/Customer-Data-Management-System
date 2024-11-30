d81966a4d6c3e45e480100b8ad552dbe39cf5041
itsfadymate <57325098+itsfadymate@users.noreply.github.com>
2024-11-30 22:04:24 +0200

    added service info model

---
+++ TelecomWebApp/Models/ServiceInfoModel.cs
@@ -0,0 +1,12 @@
+namespace TelecomWebApp.Models
+{
+    public class ServiceInfo
+{
+    public int RemainingMoney { get; set; }
+    public int ExtraMoney { get; set; }
+    public int UnresolvedTickets { get; set; }
+    public int HighestValueVoucher { get; set; }
+}
+
+
+}
