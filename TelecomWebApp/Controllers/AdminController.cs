e69eb9d3f0d7481d6655ecc1b50776562d96953a
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2024-12-02 20:42:31 +0200

    admin controller primed for part 2.1 through 2.4

--- TelecomWebApp/Controllers/AdminController.cs
+++ TelecomWebApp/Controllers/AdminController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace TelecomWebApp.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly TelecomContext _context;
+        public AdminController(TelecomContext context)
+        {
+            _context = context;
+        }
         public IActionResult Dashboard()
         {
             return View();
         }
+
+        public IActionResult CustomerWallet()
+        {
+            var walletDetails = _context.Set<dynamic>().FromSqlRaw("SELECT * FROM CustomerWallet").ToList();
+            return View(walletDetails);
+        }
+        public IActionResult E_shopVouchers()
+        {
+            var vouchers = _context.Set<dynamic>().FromSqlRaw("SELECT * FROM E_shopVouchers").ToList();
+            return View(vouchers);
+        }
+        public IActionResult AccountPayments()
+        {
+            var payments = _context.Set<dynamic>().FromSqlRaw("SELECT * FROM AccountPayments").ToList();
+            return View(payments);
+        }
+
+        public IActionResult Num_of_cashback()
+        {
+            var cashbacks = _context.Set<dynamic>().FromSqlRaw("SELECT * FROM Num_of_cashback").ToList();
+            return View(cashbacks);
+        }
+
     }
 }
+       
