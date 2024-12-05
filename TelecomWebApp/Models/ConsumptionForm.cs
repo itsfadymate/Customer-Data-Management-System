eb8f0e42f07491742c170236f031d8d908a14b81
Magdy2004 <omarmagdy11964@gmail.com>
2024-12-05 18:29:17 +0200

    added 2.1.3 input form
    .

---
+++ TelecomWebApp/Models/ConsumptionForm.cs
@@ -0,0 +1,11 @@
+﻿namespace TelecomWebApp.Models
+{
+    public class ConsumptionForm
+    {
+        public string plan_name { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+    }
+
+
+}
